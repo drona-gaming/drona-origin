ed9ea33c7f6bcb7a5c097ee0b4357be89090a0d1
drona-gaming <drona.gaming@abhishektarafdar.in>
2023-05-28 12:09:28 +0530

    update module

--- index.js
+++ index.js
@@ -12,16 +12,25 @@ const command = require('./command');
 client.on('ready', () => {
   console.log('The client is online!')
 
-client.on('guildMemberAdd', member => {
-  member.send("Welcome to 🌟DRØNÀ多Gᴀᴍɪɴɢ🌟!! Please read the <#700644963427549184> for dedicated role and full access to the server.")
-});
-
 client.user.setActivity("DRØNÀ Gᴀᴍɪɴɢ", {
-  type: "LISTENING",
+  type: "WATCHING",
 });
 });
 
-
+client.on('guildMemberAdd', member => {
+  const welcomeEmbed = new Discord.MessageEmbed()
+    .setColor('#39FF14')
+    .setTitle(`Welcome to the server, ${member.user.username}!`)
+    .setDescription('We hope you will enjoy your time here. Please read the <#700644963427549184> for dedicated role and full access to the server.')
+    .setThumbnail(member.user.displayAvatarURL())
+    .setTimestamp()
+    .setFooter('DRØNÀ Gᴀᴍɪɴɢ', 'https://i.imgur.com/FSsmpQR.png');
+  
+  member.send(welcomeEmbed)
+    .catch(error => {
+      console.error(`Could not send DM to ${member.user.tag}.\n`, error);
+    });
+});
 
   command(client, ['ping', 'test'], (message) => {
     message.reply('Calculating ping...').then((resultMessage) => {
